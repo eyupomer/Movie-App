ca014955839e580dd4935bd3bdcde702c79d2d0a
eyupomer <odabasieyupomer@gmail.com>
2022-11-25 19:39:17 +0300

    login form done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import { useState } from "react";
+import {FcGoogle} from "react-icons/fc";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="flex justify-center">
+      <div className="hidden lg:block min-w-[800px] min-h-[800px]">
+        <img
+          className="w-full h-full"
+          src={"https://picsum.photos/800/800"}
+          alt="Sample-movie"
+        />
+      </div>
+      <div className="flex-1 p-20 bg-gray-200">
+        <h1 className="text-4xl text-center mb-12">Login</h1>
+        <form id="register" onSubmit = {handleSubmit}>
+          <div className="flex flex-col gap-y-5">
+            
+            <div className="flex flex-col gap-y-1 items-start">
+              <label className="pl-6 text-2xl" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="w-10/12 mx-5 p-2 rounded-md border-2 focus:outline-none"
+                name="email"
+                id="email"
+                placeholder="Enter your email adress."
+                type="mail"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col gap-y-1 items-start">
+              <label className="pl-6 text-2xl" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="w-10/12 mx-5 p-2 rounded-md border-2 focus:outline-none"
+                name="password"
+                id="password"
+                placeholder="Enter your password."
+                type="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="text-blue-600 underline text-md cursor-pointer ml-6">Forgot Password?</div>
+            <input
+              className="mt-3 py-2 text-lg border-2 rounded-md cursor-pointer bg-blue-600 text-white w-full hover:bg-white hover:text-blue-600 duration-300 hover:border-blue-600"
+              value="Login"
+              type="submit"
+            />
+          </div>
+        </form>
+        <button className="mt-4 py-2 text-lg border-2 rounded-md bg-blue-600 text-white w-full hover:bg-white hover:text-blue-600 duration-300 hover:border-blue-600">
+          Continue with Google <FcGoogle className="inline ml-2" size={22} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 const Register = () => {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="flex justify-center">
       <div className="hidden lg:block min-w-[800px] min-h-[800px]">
@@ -10,19 +22,21 @@ const Register = () => {
       </div>
       <div className="flex-1 p-20 bg-gray-200">
         <h1 className="text-4xl text-center mb-12">Register</h1>
-        <form>
-          <div className="flex flex-col gap-y-3">
+        <form id="register" onSubmit = {handleSubmit}>
+          <div className="flex flex-col gap-y-6">
             <div className="flex flex-col gap-y-3 items-start">
               <label className="pl-6 text-2xl" htmlFor="firstName inline">
                 First Name
               </label>
               <input
-                className="w-10/12 m-5 p-2 rounded-md border-2 focus:outline-none"
+                className="w-10/12 mx-5 p-2 rounded-md border-2 focus:outline-none"
                 name="firstName"
                 id="firstName"
                 placeholder="Enter your first name."
                 type="text"
                 required
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
               />
             </div>
             <div className="flex flex-col gap-y-3 items-start">
@@ -30,12 +44,14 @@ const Register = () => {
                 Last Name
               </label>
               <input
-                className="w-10/12 m-5 p-2 rounded-md border-2 focus:outline-none"
+                className="w-10/12 mx-5 p-2 rounded-md border-2 focus:outline-none"
                 name="lastName"
                 id="lastName"
                 placeholder="Enter your last name."
                 type="text"
                 required
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
               />
             </div>
             <div className="flex flex-col gap-y-3 items-start">
@@ -43,12 +59,14 @@ const Register = () => {
                 Email
               </label>
               <input
-                className="w-10/12 m-5 p-2 rounded-md border-2 focus:outline-none"
+                className="w-10/12 mx-5 p-2 rounded-md border-2 focus:outline-none"
                 name="email"
                 id="email"
                 placeholder="Enter your email adress."
                 type="mail"
                 required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="flex flex-col gap-y-1 items-start">
@@ -56,16 +74,18 @@ const Register = () => {
                 Password
               </label>
               <input
-                className="w-10/12 m-5 p-2 rounded-md border-2 focus:outline-none"
+                className="w-10/12 mx-5 p-2 rounded-md border-2 focus:outline-none"
                 name="password"
                 id="password"
                 placeholder="Enter your password."
                 type="password"
                 required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <input
-              className="px-3 py-1 text-lg border-2 rounded-md cursor-pointer bg-blue-600 text-white w-full hover:bg-white hover:text-blue-600 duration-300 hover:border-blue-600"
+              className="py-2 text-lg border-2 rounded-md cursor-pointer bg-blue-600 text-white w-full hover:bg-white hover:text-blue-600 duration-300 hover:border-blue-600"
               value="Register"
               type="submit"
             />
